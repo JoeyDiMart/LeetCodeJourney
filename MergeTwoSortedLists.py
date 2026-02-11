d6113cfd81b2314d624f9715f6791142bbb22d51
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2026-02-10 23:40:10 -0500

    small updates + fix for Pycharm

--- MergeTwoSortedLists.py
+++ MergeTwoSortedLists.py
@@ -1,11 +1,11 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution(object):
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
     def mergeTwoLists(self, list1, list2):
-        def add(merged_lists, Node):
+        def add(list, Node):
             pass
 
         """
@@ -15,23 +15,23 @@ class Solution(object):
         """
         merged_lists = ListNode()
 
-        if list1 == None and list2 == None:
+        if list1 is None and list2 is None:
             return []
-        elif list1 == None:
+        elif list1 is None:
             return list2
-        elif list2 == None:
+        elif list2 is None:
             return list1
 
         while True:
-            if list1.val == None:
-                if list2.val == None:
+            if list1.val is None:
+                if list2.val is None:
                     return merged_lists
                 else:
                     add(merged_lists, list2.val)
                     continue
-            elif list2.val == None:
+            elif list2.val is None:
                 add(merged_lists, list1.val)
                 continue
 
-            if list1.val <= list2(list2_point).val:
+            if list1.val <= list2.val:
                 add(merged_lists, list1.val)
