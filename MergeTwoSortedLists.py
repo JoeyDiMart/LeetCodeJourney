9a786c37ae29c456893b0aa166216b0eb0dd7fd0
JoeyDiMart <joseph.dimartino@spartans.ut.edu>
2026-02-11 10:03:00 -0500

    Update MergeTwoSortedLists.py

--- MergeTwoSortedLists.py
+++ MergeTwoSortedLists.py
@@ -1,37 +1,38 @@
 # Definition for singly-linked list.
-class ListNode(object):
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
     def mergeTwoLists(self, list1, list2):
-        def add(list, Node):
-            pass
 
         """
         :type list1: Optional[ListNode]
         :type list2: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        merged_lists = ListNode()
+        temp = ListNode()
+        curr = temp
 
         if list1 is None and list2 is None:
-            return []
+            return None
         elif list1 is None:
             return list2
         elif list2 is None:
             return list1
 
-        while True:
-            if list1.val is None:
-                if list2.val is None:
-                    return merged_lists
-                else:
-                    add(merged_lists, list2.val)
-                    continue
-            elif list2.val is None:
-                add(merged_lists, list1.val)
-                continue
-
+        while list1 is not None and list2 is not None:
             if list1.val <= list2.val:
-                add(merged_lists, list1.val)
+                curr.next = list1
+                list1 = list1.next
+            else:
+                curr.next = list2
+                list2 = list2.next
+            curr = curr.next
+        if list1 is not None:
+            curr.next = list1
+        else:
+            curr.next = list2
+
+        return temp.next
+
